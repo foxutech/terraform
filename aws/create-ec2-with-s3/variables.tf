a7797cfdcb77cfbf38ce0145ad33d8e3bcdea478
FoxuTech <prabulugar@gmail.com>
2019-01-14 11:59:19 +0530

    Update variables.tf

--- aws/create-ec2-with-s3/variables.tf
+++ aws/create-ec2-with-s3/variables.tf
@@ -1,10 +1,3 @@
 variable "region" {
   default = "us-east-1"
 }
-variable "AmiLinux" {
-  type = "map"
-  default = {
-    us-east-1 = "ami-b73b63a0" # Virginia
-  }
-  description = "have only added one region"
-}
